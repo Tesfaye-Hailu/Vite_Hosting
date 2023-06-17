c8cf5188b2f0da18ae3010ef2835201fadac1dfc
Tesfaye Hailu <thailu@LABA.PSWAS.LOCAL>
2023-06-16 18:16:20 -0700

    added type

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { createApp, ref } from 'vue';
 
 function Bader() {
   return (
     <div>
         <h1>This Tesfaye's page</h1></div>
+       
+
+//         const app = createApp({
+//           setup() {
+//             const todos = ref([]);
+//             const newTodo = ref('');
+        
+//             const addTodo = () => {
+//               if (newTodo.value !== '') {
+//                 todos.value.push(newTodo.value);
+//                 newTodo.value = '';
+//               }
+//             };
+        
+//             const deleteTodo = (index) => {
+//               todos.value.splice(index, 1);
+//             };
+        
+//             return {
+//               todos,
+//               newTodo,
+//               addTodo,
+//               deleteTodo,
+//             };
+//           },
+//         });
+        
+//         app.mount('#app');
   )
 }
 
